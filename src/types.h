6d9cf111b20256ce82db84d4709e0368e07c3886
shimstock22 <113155609+shimstock22@users.noreply.github.com>
2026-09-21 20:01:27 -0500

    Fix MotS puppet submode array size

--- src/types.h
+++ src/types.h
@@ -1350,14 +1350,7 @@ typedef struct SithPuppetClassSubmode
 
 typedef struct sithAnimclassMode
 {
-    SithPuppetClassSubmode keyframe[42];
-    uint32_t field_2A0;
-    uint32_t field_2A4;
-    uint32_t field_2A8;
-    uint32_t field_2AC;
-#ifdef JKM_TYPES
-    uint32_t pad[8];
-#endif
+    SithPuppetClassSubmode keyframe[45];
 } sithAnimclassMode;
 
 typedef struct SithPuppetClass
